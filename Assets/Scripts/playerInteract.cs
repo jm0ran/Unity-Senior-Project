005ea92bba992f7a4c36d5446470879142c1f1be
Joseph Moran <josephdeargmoran@Gmail.com>
2021-10-06 11:42:22 -0400

    Interaction Range and Collision Detection

--- Assets/Scripts/playerInteract.cs
+++ Assets/Scripts/playerInteract.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class playerInteract : MonoBehaviour
 {
+    public GameObject currentInterObj;
+
     void OnTriggerEnter2D(Collider2D other){ //Runs when there is a collision between a trigger and regular collider
-        Debug.Log("Collision");
-        Debug.Log(other.tag);
+        if (other.tag == "Interactable"){
+            currentInterObj = other.gameObject; //Stores collided object assuming it is tagged "Interactable"
+        }
     }
+    void OnTriggerExit2D(Collider2D other){ //On exit with a trigger collision area
+        if (other.tag == "Interactable" && other.gameObject == currentInterObj){
+            currentInterObj = null; //Resets the object assuming it's equal to the current game object
+        }
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.E) && currentInterObj != null){ //Checks for 
+            Debug.Log("Interacted with something");
+        }
+    }
+
     void Start(){
         Debug.Log("playerInteract Script Started");
     }
