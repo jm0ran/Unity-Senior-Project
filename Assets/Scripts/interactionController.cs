aae74a6e8bf853c3381d77559028574b765af5d9
Joseph Moran <josephdeargmoran@Gmail.com>
2021-10-07 10:05:08 -0400

    Organization + Interaction Prep
    Player becomes locked upon interaction preparing to begin rendering text

--- Assets/Scripts/interactionController.cs
+++ Assets/Scripts/interactionController.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class interactionController : MonoBehaviour
 {
     public float interactionRange; //Want to implement later
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public bool talks;
+    public bool item;
+
+
+    void renderSpeech(){
+        //This will be what creates and renders the dialougue, want to seperate the speech function
+
+        //Steps:
+
+        //Enter a locked state where player cannot make any actions
+        //Intend to take a list with strings of text split up for dialouge differences and cycle through waiting for the next keyprompt to continue
+        //After cycling through speech, check if the interactable object has an item and if so give it to the player
+        //Reenable player movement
+
+
+
     }
 
-    void printHi(string message){
+
+    void printHi(GameObject player){
         gameObject.SetActive(false);
-        Debug.Log(message);
+        player.SendMessage("lockPlayer", true);
     }
 
     // Update is called once per frame
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -15,92 +15,122 @@ public class playerController : MonoBehaviour
     public float verticalSpeed;
     public float totSpeed;
 
+    //Used for interaction
+    public GameObject currentInterObj;
+    public bool locked = false;
+
     //Slighly different from start but also runs before the game starts
     void Awake(){
         animator = GetComponent<Animator>(); //Grabs the animator component from the object that this script is attached to
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     //Fixed refresh time seperate from framerate
     void FixedUpdate()
     {
         checkMovement();
+        checkActions();
+        
     }
+
+    //Will be used to lock player movement and actions during dialouge and the sorts
+    void lockPlayer(bool state){
+        locked = state;
+        movement.x = 0;
+        movement.y = 0;
+    }
+
+
+    //Used to check actions for non movement related tasks
+    void checkActions()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && currentInterObj != null && !locked){ //Checks for keypress as well as that you are in range of an interactable object
+            currentInterObj.SendMessage("printHi", gameObject);
+        }
+    }
+
 
     //Move function accepts different directions
     void checkMovement()
     {
-        //I want to be careful of this code here, dont want it to cause jitters, it should but idk
-        movement.y = 0;
-        movement.x = 0;
+        if(!locked)
+        {
+            //I want to be careful of this code here, dont want it to cause jitters, it should but idk
+            movement.y = 0;
+            movement.x = 0;
 
-        //Heres the basic movement functions, very simple move controller, I want to lock into one direction, but am going to work with animator first
-        
-        //This is the entrance point for movement, the system is designed so you can only move in one direction at a time
-        if(!turnLocked){
-            if(Input.GetKey("up"))
-            {
-                movement.y = 1;
-                lastKey = "up";
-            }else if(Input.GetKey("down"))
-            {
-                movement.y = -1;
-                lastKey = "down";
-            }else if(Input.GetKey("left"))
-            {
-                movement.x = -1;
-                lastKey = "left";
-            }else if(Input.GetKey("right"))
-            {
-                movement.x = 1;
-                lastKey = "right";
-            }
-
-            //Locks turning
-            if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")){
-                turnLocked = true;
-            }
-        }
-        //Triggered once a key has been held down, just lets you continue in that general direction
-        else{
-            if(Input.GetKey(lastKey)){
-                if(lastKey == "up"){
-                    movement.y = 1;
-                }else if(lastKey == "down"){
-                    movement.y = -1;
-                }else if(lastKey == "left"){
-                    movement.x = -1; 
-                }else if(lastKey == "right"){
-                    movement.x = 1;
-                }
+            //Heres the basic movement functions, very simple move controller, I want to lock into one direction, but am going to work with animator first
             
-            }else{
-                turnLocked = false;
+            //This is the entrance point for movement, the system is designed so you can only move in one direction at a time
+            if(!turnLocked){
+                if(Input.GetKey("up"))
+                {
+                    movement.y = 1;
+                    lastKey = "up";
+                }else if(Input.GetKey("down"))
+                {
+                    movement.y = -1;
+                    lastKey = "down";
+                }else if(Input.GetKey("left"))
+                {
+                    movement.x = -1;
+                    lastKey = "left";
+                }else if(Input.GetKey("right"))
+                {
+                    movement.x = 1;
+                    lastKey = "right";
+                }
+
+                //Locks turning
+                if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")){
+                    turnLocked = true;
+                }
             }
+            //Triggered once a key has been held down, just lets you continue in that general direction
+            else{
+                if(Input.GetKey(lastKey)){
+                    if(lastKey == "up"){
+                        movement.y = 1;
+                    }else if(lastKey == "down"){
+                        movement.y = -1;
+                    }else if(lastKey == "left"){
+                        movement.x = -1; 
+                    }else if(lastKey == "right"){
+                        movement.x = 1;
+                    }
+                
+                }else{
+                    turnLocked = false;
+                }
+            }
+
+            //Below is where we define floats that the animation controller uses to manage states
+            horizontalSpeed = movement.x * moveSpeed;
+            verticalSpeed = movement.y * moveSpeed ;
+            totSpeed = horizontalSpeed + verticalSpeed;
+            animator.SetFloat("horizontalSpeed", horizontalSpeed);
+            animator.SetFloat("verticalSpeed", verticalSpeed);
+            animator.SetFloat("totSpeed", totSpeed);
+
+
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         }
-
-        //Below is where we define floats that the animation controller uses to manage states
-        horizontalSpeed = movement.x * moveSpeed;
-        verticalSpeed = movement.y * moveSpeed ;
-        totSpeed = horizontalSpeed + verticalSpeed;
-        animator.SetFloat("horizontalSpeed", horizontalSpeed);
-        animator.SetFloat("verticalSpeed", verticalSpeed);
-        animator.SetFloat("totSpeed", totSpeed);
-
-
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
     }
+
+    //Interaction Section ---------------------------------------------------------------------------
+    void OnTriggerEnter2D(Collider2D other){ //Runs when there is a collision between a trigger and regular collider
+        if (other.tag == "Interactable"){
+            currentInterObj = other.gameObject; //Stores collided object assuming it is tagged "Interactable"
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){ //On exit with a trigger collision area
+        if (other.tag == "Interactable" && other.gameObject == currentInterObj){
+            currentInterObj = null; //Resets the object assuming it's equal to the current game object
+        }
+    }
+
+
+
+
+
 }
 
