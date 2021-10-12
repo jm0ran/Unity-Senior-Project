afe68dd780f6c3ee857277c9f436afa8d2cd31cd
Joseph Moran <josephdeargmoran@Gmail.com>
2021-10-12 10:47:57 -0400

    Prefabs, Camera, and Scene Work
    -Created Prefabs for Camera, Player, NPC, and UI
    -Began to research and find what I need to begin to bound camera movement
    -Put together basics for new Scene in the trash cave the main character will reside in
    -Cleaned up some code

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,11 +6,16 @@ public class cameraController : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public Camera cam; 
+    public GameObject map;
+    public SpriteRenderer mapSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = gameObject.GetComponent<Camera>();
+        map = GameObject.FindWithTag("Map");
+        mapSprite = map.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,4 +24,9 @@ public class cameraController : MonoBehaviour
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
+    void FixedUpdate(){
+        //Going to try to use this to bound the camera from going off the map
+        //Debug.Log(cam.orthographicSize); 
+        Debug.Log(mapSprite.bounds.extents.x);
+    }
 }
