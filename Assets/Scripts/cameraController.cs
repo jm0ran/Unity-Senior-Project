7484cade1b29507d119bc965c13be52d7408baa0
Joseph Moran <josephdeargmoran@Gmail.com>
2021-10-12 12:06:56 -0400

    Camera bounded by map

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -9,6 +9,8 @@ public class cameraController : MonoBehaviour
     public Camera cam; 
     public GameObject map;
     public SpriteRenderer mapSprite;
+    public float[] mapBoundaries;
+    public float[] newCameraCords;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +18,34 @@ public class cameraController : MonoBehaviour
         cam = gameObject.GetComponent<Camera>();
         map = GameObject.FindWithTag("Map");
         mapSprite = map.GetComponent<SpriteRenderer>();
+
+        //Create arrays
+        mapBoundaries = new float[2];
+        newCameraCords = new float[2];
+
+        //Assign the Map Boundaries, 0 is horizontal reach, 1 is vertical
+        mapBoundaries[0] = mapSprite.bounds.extents.x;
+        mapBoundaries[1] = mapSprite.bounds.extents.y;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if(cam.orthographicSize * cam.aspect + Mathf.Abs(player.position.x) < mapBoundaries[0]){
+            newCameraCords[0] = player.position.x;
+        }else{
+            newCameraCords[0] = transform.position.x;
+        }
+        if (cam.orthographicSize + Mathf.Abs(player.position.y) < mapBoundaries[1]){
+            newCameraCords[1] = player.position.y;
+        }else{
+            newCameraCords[1] = transform.position.y;
+        }
+
+        transform.position = new Vector3(newCameraCords[0], newCameraCords[1], transform.position.z);
+        
     }
 
-    void FixedUpdate(){
-        //Going to try to use this to bound the camera from going off the map
-        //Debug.Log(cam.orthographicSize); 
-        Debug.Log(mapSprite.bounds.extents.x);
-    }
 }
