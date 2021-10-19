c9eb7cb126ec9ce9d39c8dc5ff9ace56bd03db39
Joseph Moran <josephdeargmoran@Gmail.com>
2021-10-19 10:44:13 -0400

    Added in more profiles and prepared UI Controller for dynamic character icons during dialogue

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     //Want to use UI Controller to control what is being shown in the UI stuff like menus and Dialogue for when I have more than just textboxs
     //------------------------------------------------------------------------
     //Main variables used in Script
+    //The character profiles being used are stored in these Sprite variables
+    public Sprite mainProfile;
+    public Sprite YeProfile;
+    public Sprite gokuProfile;
+    public Sprite hunterProfile;
+    public Sprite banditProfile;
+    public Sprite imposterProfile;
+    
+    public Image profileBox;
 
     //------------------------------------------------------------------------
     //User defined functions
+    void changeProfile(){
+        //This is where we're going to be changinng the profile photo for the charcter window
+    }
+    void changeText(){
+        //Want to move the functione used to update the text on the screen to here
+    }
 
     //------------------------------------------------------------------------
     //Unity Defined Functions
     void Start()
     {
+        //Very initial testing of just how changing the image works in unity and stuff preparing for a system to change during dialougue
+        profileBox = GameObject.FindWithTag("profileBox").GetComponent<Image>();
+        profileBox.sprite = gokuProfile;
         gameObject.SetActive(false); //Disables object initially so that the UI is not visable on the start of a Scene
     }    
 }
