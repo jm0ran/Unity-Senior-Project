7e0146e5080159f55dc329c5dc9cfbb6a7ad0990
Joseph Moran <josephdeargmoran@Gmail.com>
2021-11-05 09:58:17 -0400

    Timing is right kind of, gotta rework my AudioTestController (Will rename this eventually)

--- Assets/Scripts/audioTestController.cs
+++ Assets/Scripts/audioTestController.cs
@@ -7,34 +7,46 @@ public class audioTestController : MonoBehaviour
     public AudioSource testAudio;
     public GameObject arrowPrefab;
     public beatMap mainMap;
+    public float delayStart;
+    public float targetX;
+    public float noteSpawnPos;
+    private float timeToTarget;
 
     //User Defined Functions
     void visualizeNote(){
-        if(mainMap.map.Count > 0 && testAudio.time > mainMap.map[0].time){
+        Debug.Log((mainMap.map[0].time + delayStart - timeToTarget) - Time.timeSinceLevelLoad);
+        
+        if(mainMap.map.Count > 0 && (Time.timeSinceLevelLoad) > (mainMap.map[0].time + delayStart - timeToTarget)){
             newArrow(mainMap.map[0].time, mainMap.map[0].button);
-            mainMap.map.RemoveAt(0);
         }
     }
 
     void newArrow(float triggerTime, string button){ //This is my function that is going to instantiate my arrow
+        mainMap.map.RemoveAt(0);
         GameObject newArrow = Instantiate(arrowPrefab); //This is what creates an arrow, this is also what I'll be doing with my script
-        newArrow.transform.position = new Vector3(3,2,0); //Want to change this based on note type
+        newArrow.transform.position = new Vector3(noteSpawnPos,2,0); //Want to change this based on note type
         noteController newArrowNC = newArrow.GetComponent<noteController>(); 
         newArrowNC.triggerTime = triggerTime;
         newArrowNC.button = button;
         
     }
 
+    IEnumerator startMusic(){
+        yield return new WaitForSeconds(delayStart);
+        //Starts audio
+        testAudio.Play();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        //Creates and starts audio
+        timeToTarget = (noteSpawnPos - targetX) / (0.1f * 60f);
         testAudio = gameObject.GetComponent<AudioSource>();
-        testAudio.Play();
-
         //Imports the beatMap's json file which holds the information on each note
         mainMap = new beatMap();
         mainMap.readBeatMap("Runaway.json");
+        StartCoroutine(startMusic());
+       
     }
 
     // Update is called once per frame
@@ -53,5 +65,4 @@ public class audioTestController : MonoBehaviour
     void FixedUpdate(){
         //Debug.Log(testAudio.time);
     }
-
 }
--- Assets/Scripts/noteController.cs
+++ Assets/Scripts/noteController.cs
@@ -8,10 +8,14 @@ public class noteController : MonoBehaviour
     public float triggerTime;
     public string button;
     public Rigidbody2D rb;
+    public float speed = 1;
 
     //User Defined Functions
     public void moveNote(){
-        rb.MovePosition(new Vector2(rb.position.x - 0.1f,rb.position.y));
+        if(rb.position.x < -7.5){
+            Destroy(gameObject);
+        }
+        //rb.MovePosition(new Vector2(rb.position.x - 0.1f,rb.position.y) * Time.deltaTime);
     }
 
 
@@ -22,6 +26,9 @@ public class noteController : MonoBehaviour
     }
     public void Awake(){
         rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update(){
+        transform.Translate(Vector3.left * (10.5f / 1.75f) * Time.deltaTime);
     }
 
 
