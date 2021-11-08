eec0cb70eb95aefb6391fbdd9856ad73b4fc0460
Joseph Moran <josephdeargmoran@Gmail.com>
2021-11-08 07:49:20 -0500

    Destroy Notes on top row

--- Assets/Scripts/rythmInputController.cs
+++ Assets/Scripts/rythmInputController.cs
@@ -6,10 +6,29 @@ public class rythmInputController : MonoBehaviour
 {
     private List<GameObject> currentNotes = new List<GameObject>();
 
+
+
+//User Defined Functions
+    void triggerCurrentNote(){
+        if(currentNotes.Count > 0){ //The closest note to the player is going to be 0 in the array, besides for the possible edgecase of a passed note, but thats an edgecase to work on later
+            Destroy(currentNotes[0]);
+            currentNotes.RemoveAt(0);
+        }else{
+            
+        }
+    }
+
+
+
+//Unity Defined Functions
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "arrow"){
             currentNotes.Add(other.gameObject);
-            Debug.Log(currentNotes.Count);
+        }
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.UpArrow)){ //This will need to be changed to be dynamic for different directions
+            triggerCurrentNote();
         }
     }
 }
