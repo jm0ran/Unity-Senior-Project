e3cc0175feea7a31a58065506882ec10c525164b
Joseph Moran <josephdeargmoran@Gmail.com>
2021-11-12 10:45:05 -0500

    Bush go bye bye

--- Assets/Scripts/chestController.cs
+++ Assets/Scripts/chestController.cs
@@ -6,14 +6,20 @@ public class chestController : MonoBehaviour
 {
     //Variables for the program
     public GameObject player;
-
+    public Sprite altSprite;
+    public bool triggered = false;
 
     //User defined functions
     public void openChest(){
         //This is where I want to open the chest and pass all my items to my player
         //I want to be checking for stacks and just generally want to be interacting with the inventory controller and inventory in general which should be instantiated inside the player script
+        if(altSprite != null && !triggered){
+            gameObject.GetComponent<SpriteRenderer>().sprite = altSprite;
+            triggered = true;
+        }
         Debug.Log("Triggered an openChest");
     }
+
 
     //Unity Defined functions
     public void Awake(){
