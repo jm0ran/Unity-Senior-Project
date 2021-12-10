c46780c8dd2a57c1f5d3a71e9703574d2038dc4b
Joseph Moran <josephdeargmoran@protonmail.com>
2021-12-10 10:16:08 -0500

    Temporary Layout and Assets for Opening Scene

--- Assets/Scripts/Cutscene Stage/cutsceneController.cs
+++ Assets/Scripts/Cutscene Stage/cutsceneController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cutsceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI sceneText;
+
+
     void Start()
     {
-        
+        sceneText = GameObject.FindWithTag("textBox").GetComponent<TextMeshProUGUI>();
+        sceneText.text = "Hi";
     }
 
     // Update is called once per frame
