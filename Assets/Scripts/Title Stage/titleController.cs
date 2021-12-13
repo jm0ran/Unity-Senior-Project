1eae6ce0c2d047cebc5fd9a7b2ad98beae599d15
Joseph Moran <josephdeargmoran@protonmail.com>
2021-12-13 11:47:35 -0500

    Preparing Sliding Background

--- Assets/Scripts/Title Stage/titleController.cs
+++ Assets/Scripts/Title Stage/titleController.cs
@@ -6,20 +6,16 @@ using UnityEngine.SceneManagement;
 public class titleController : MonoBehaviour
 {
     public GameObject flashingText;
+    public Transform backing1;
+    public Transform backing2;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        flashingText = GameObject.FindWithTag("flashingText");
-        StartCoroutine(startFlashingText());
-    }
-
-    void Update(){
-        if(Input.GetKeyDown(KeyCode.Return)){
-            startGame();
+    IEnumerator slideBacking(){
+        for(int i = 0; i < 200000; i++){
+            backing1.position = backing1.position + new Vector3(0.5f,0,0);
+            backing2.position = backing2.position + new Vector3(0.5f,0,0);
+            yield return new WaitForSeconds(0.005f);
         }
     }
-
 
     IEnumerator startFlashingText(){
         while(true){
@@ -32,5 +28,24 @@ public class titleController : MonoBehaviour
 
     void startGame(){
         SceneManager.LoadScene("Cutscene 1");
+    }
+
+
+
+
+
+    void Start()
+    {
+        flashingText = GameObject.FindWithTag("flashingText");
+        backing1 = GameObject.FindWithTag("backing1").GetComponent<Transform>();
+        backing2 = GameObject.FindWithTag("backing2").GetComponent<Transform>();
+        StartCoroutine(startFlashingText());
+        StartCoroutine(slideBacking());
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Return)){
+            startGame();
+        }
     }
 }
