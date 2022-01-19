9555e33438772ca19f7be3a8c6a9a425d919c388
Joseph Moran <josephdeargmoran@protonmail.com>
2022-01-19 09:46:49 -0500

    Class Hell Never Ends 👿

---
+++ Assets/Scripts/InterScene/saveDataController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveDataController : MonoBehaviour
+{
+    public static Save globalSave;
+
+    void Start(){
+        globalSave = new Save();
+        globalSave.loadSavaData();
+        globalSave.serializeSaveData();
+    }
+}
