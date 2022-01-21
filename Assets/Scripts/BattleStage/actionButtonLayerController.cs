e1819f909262b01111b44332748b3a157ed5e761
Joseph Moran <josephdeargmoran@protonmail.com>
2022-01-21 09:24:39 -0500

    Dictionary and switch character prep 📚

--- Assets/Scripts/BattleStage/actionButtonLayerController.cs
+++ Assets/Scripts/BattleStage/actionButtonLayerController.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class actionButtonLayerController : MonoBehaviour
 {
     //Layers of the menu
     private GameObject rootLayer;
-    public GameObject fightLayer;
-
-    public static GameObject currentLayer;
-
+    private GameObject fightLayer;
+    private GameObject switchLayer;
+    private string currentChar;
+    private GameObject currentLayer;
+    private GameObject currentCharProfile;
     private GameObject[] layers;
+    
+    public Sprite altSprite;
+    
 
     void Start(){
         rootLayer = GameObject.Find("rootLayer");
         fightLayer = GameObject.Find("fightLayer");
+        switchLayer = GameObject.Find("switchLayer");
+        currentCharProfile = GameObject.Find("currentCharProfile");
         fightLayer.SetActive(false);
-        layers = new GameObject[] {rootLayer, fightLayer};
+        switchLayer.SetActive(false);
+        layers = new GameObject[] {rootLayer, fightLayer, switchLayer};
+        currentChar = saveDataController.globalSave.currentTeam[0];
+        updateCharProfile(currentChar);
+    }
+
+    void updateCharProfile(string newChar){
+        currentCharProfile.SendMessage("switchProfile", currentChar);
     }
 
     void processButton(string trigger){
         if(trigger == "fight"){
             menuTransition(fightLayer);
+        }else if(trigger == "switch"){
+            menuTransition(switchLayer);
         }
     }
 
---
+++ Assets/Scripts/BattleStage/charProfileImageController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class charProfileImageController : MonoBehaviour
+{
+    //Def a better way to do this that future me will find
+    public Sprite Kanye;
+    public Sprite Goku;
+    public Sprite Imposter;
+
+    private Image profile;
+
+    public static Dictionary<string, Sprite> spriteDictionary = new Dictionary<string, Sprite>();
+
+    void Start(){
+        profile = gameObject.GetComponent<Image>();
+        spriteDictionary.Add("Kanye", Kanye);
+        spriteDictionary.Add("Goku", Goku);
+        spriteDictionary.Add("Imposter", Imposter);
+    }
+
+    void switchProfile(string charName){
+        profile.sprite = spriteDictionary[charName];
+    }
+}
