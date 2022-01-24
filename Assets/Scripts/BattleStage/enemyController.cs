73119b2e11b630d2fd0d46ba0eccbf8ef2171888
Joseph Moran <josephdeargmoran@protonmail.com>
2022-01-24 13:51:52 -0500

    Enemy Dictionary

--- Assets/Scripts/BattleStage/enemyController.cs
+++ Assets/Scripts/BattleStage/enemyController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class enemyController : MonoBehaviour
 {
+   public string enemy = "Drake";
+
    void Start(){
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
    }
