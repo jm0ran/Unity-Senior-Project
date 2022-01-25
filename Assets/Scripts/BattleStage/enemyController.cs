68b56160fb5d0a63f9368a17bc4ad7296ce51927
Joseph Moran <josephdeargmoran@protonmail.com>
2022-01-25 08:03:44 -0500

    More enemy implementation
    some unity defined sprite disappeared earlier which is kinda worrying but we al good... probably

--- Assets/Scripts/BattleStage/enemyController.cs
+++ Assets/Scripts/BattleStage/enemyController.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class enemyController : MonoBehaviour
 {
-   public string enemy = "Drake";
+   public string enemy = "Drake"; //Going to be set dynamically in the future
+   private SpriteRenderer enemySprite;
+   private TextMeshProUGUI enemyNameTextBox;
 
-   void Start(){
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+   void Awake(){
+        loadEnemyInfo();
+   }
+
+   void loadEnemyInfo(){
+      gameObject.GetComponent<SpriteRenderer>().enabled = false;
+      enemySprite = GameObject.Find("enemyObject").GetComponent<SpriteRenderer>();
+      enemyNameTextBox = GameObject.Find("enemyName").GetComponent<TextMeshProUGUI>();
+
+      if(enemy != ""){
+         try{
+            enemySprite.sprite = charProfileImageController.enemyDictionary[enemy];
+         }catch{
+            Debug.Log("Failed to fetch a sprite for: " + enemy + " from the enemy dictionary");
+         }
+      }
+
+      enemyNameTextBox.text = enemy;
+
    }
 
    
