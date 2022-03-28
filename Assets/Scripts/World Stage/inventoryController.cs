e30fe121504aaa60b81754c80b15935ce5fabce6
Joseph Moran <josephdeargmoran@protonmail.com>
2022-03-27 22:15:45 -0400

    Color change on selection change
    Still need to add movement and item descriptions

--- Assets/Scripts/World Stage/inventoryController.cs
+++ Assets/Scripts/World Stage/inventoryController.cs
@@ -28,7 +28,11 @@ public class inventoryController : MonoBehaviour
 
     void menuInput(string inputPassed){
         if(inAction){
-            Debug.Log(inputPassed);
+            if(inputPassed == "down"){
+                updateSelected(currentSelection + 1);
+            }else if(inputPassed == "up"){
+                updateSelected(currentSelection - 1);
+            }
             //Going to use this input to scroll through basically
         }
     }
@@ -42,21 +46,29 @@ public class inventoryController : MonoBehaviour
         }
     }
 
-    public void updateSelected(){ //This function is just going to clearly highlight the currently selected item and if needed move the objects up in order to  view and stuff
+    public void updateSelected(int target){ //This function is just going to clearly highlight the currently selected item and if needed move the objects up in order to  view and stuff
         //Design for this is as follows
         //1. Get list of the item objects
         //2. Obviously figure out which is selected based on current selection index
         //3. possibly move the objects in order to slide for the menu, may need to dynamically speed or just time the transition with coroutines in order to make it less jumpy
         //4. Initial implementation will likely just be skippy
         //5. write the necessary text in the funny box
+        if(target >= 0 & target < itemObjectsList.Count){
+            GameObject selectedObject = itemObjectsList[target]; //Might need to add something here if inventory has less than 1 item who knows lol
+            
+            //I want a section here to determine if the items need to be moves down or up with funny algorithm of if and butts and coconuts
+            
+            foreach(GameObject item in itemObjectsList){ //Set all object colors to normal, also is going to handle jump menu movement
+                item.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            }
+            selectedObject.GetComponent<Image>().color = new Color32(255, 112, 112, 100); //Set selected gameObject to different color
+            currentSelection = target; //Updates current selection to reflect a successful change
+            
 
-        GameObject selectedObject = itemObjectsList[currentSelection]; //Might need to add something here if inventory has less than 1 item who knows lol
-        selectedObject.SetActive(false);        
-        
-        //Just going to disable the object temporarily to represent the chosen one
-        
 
-
+        }else{//This error handle needs to be a bit more sophisticated
+            Debug.Log("Target of " + target + " is not real bozo");
+        }
     }
 
     public void initController(){
@@ -75,7 +87,7 @@ public class inventoryController : MonoBehaviour
                 }
             }
         
-        updateSelected(); //This is the initial update for selected, just initially highlights the first option
+        updateSelected(currentSelection); //This is the initial update for selected, just initially highlights the first option
     }
 
 //------------------------------------------------------------------------
