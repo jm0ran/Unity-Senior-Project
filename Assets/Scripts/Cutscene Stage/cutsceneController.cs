5958f9366d6c66439d2ce04bf4f593619e4db1fd
Joseph Moran <josephdeargmoran@protonmail.com>
2022-05-23 10:09:59 -0400

    Small Fixes all around

--- Assets/Scripts/Cutscene Stage/cutsceneController.cs
+++ Assets/Scripts/Cutscene Stage/cutsceneController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class cutsceneController : MonoBehaviour
 {
@@ -10,6 +11,16 @@ public class cutsceneController : MonoBehaviour
     public float textDelay;
     public bool locked; //Used to lock checking to move forward while new text is loading
     public GameObject fadeShade;
+    public Image cutsceneImage;
+
+    //Storage for cutscene images
+    public Sprite scene0;
+    public Sprite scene1;
+    public Sprite scene2;
+    public Sprite scene3;
+    public Sprite scene4;
+
+    
 
     //I want this this first cutscene controller to almost serve as a template as while I only plan to have this opening cutscene plans could definitely change, going to hardcode the information in slightly
 
@@ -43,16 +54,26 @@ public class cutsceneController : MonoBehaviour
        
     IEnumerator mainLoop(){ //This is the main loop which just kind of defines the flow of everything, is going to be very hard coded but thats kind of what I'm going for right now, ordered like this so I can trigger other functions within the progression which I would be unable to do in a for loop
         //Every one of these while loops will pause the functions progression to create a pathway for the dialogue to follow
+        renderText("July 21, 2022");
+        yield return new WaitForSeconds(textDelay);
+        while(!Input.GetKeyDown(KeyCode.Return) || locked){
+            yield return null;
+        }
+        
         renderText("Detroit Michigan");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+
+        cutsceneImage.sprite = scene1;
         renderText("Kanye: What are you doing, aren’t you supposed to be back in Canada?!");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+
+        cutsceneImage.sprite = scene2;
         renderText("Unknown Male: Plans changed…");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
@@ -68,21 +89,28 @@ public class cutsceneController : MonoBehaviour
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+        cutsceneImage.sprite = scene3;
         renderText("Kanye: We?!");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+
+        cutsceneImage.sprite = scene2;
         renderText("Unknown Female: It’s time to end this Bad Blood Kanye");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+
+        cutsceneImage.sprite = scene0;
         renderText("On July 21st, 2022, Kanye West dissapeared.");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
             yield return null;
         }
+        
+        cutsceneImage.sprite = scene4;
         renderText("Almost a decade later many have forgotten about Ye, but not all have given up hope…");
         yield return new WaitForSeconds(textDelay);
         while(!Input.GetKeyDown(KeyCode.Return) || locked){
@@ -95,11 +123,16 @@ public class cutsceneController : MonoBehaviour
 
     //---------------------------------------------------------------------------------
     //Unity Defined Functions
-    void Start()
+    void Awake()
     {
         sceneText = GameObject.FindWithTag("textBox").GetComponent<TextMeshProUGUI>();
         fadeShade = GameObject.FindWithTag("fadeShade");
+        cutsceneImage = GameObject.Find("cutsceneImage").GetComponent<Image>();
 
+        
+    }
+
+    void Start(){
         StartCoroutine(mainLoop());
     }
 
